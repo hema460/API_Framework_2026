4d7b134616a49e57f20f420b1fe783a9a1169703
hema <hemajayanthreddy@gmail.com>
2026-02-18 18:03:34 -0600

    added another line for webhook

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -36,6 +36,7 @@ public class UserTests {
     @Test(priority = 0)
     public  void testPostUser() throws IOException {
 //using routes class where giving base uri and endpoints
+        //this is a post user
         logger.debug("***************** creating user *************************");
         Response response= UserEndPoints.createUser(userPayload);
         response.then().log().all();
